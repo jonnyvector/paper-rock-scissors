9805902a83f3b5d9ffd390ca09fc9af891e9d493
JONATHAN HICKS <jonny@teachon.org>
2023-10-21 16:25:44 +0700

    created compareChoices function

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
-function getComputerChoice() {
+const getComputerChoice = () => {
   const randomNumber = Math.floor(Math.random() * 3);
-  const gameChoicesArray = ["Rock", "Paper", "Scissors"];
-  return gameChoicesArray[randomNumber];
+  const gameChoicesArray = ["rock", "paper", "scissors"];
+  const computerChoice = gameChoicesArray[randomNumber];
+  console.log(computerChoice);
+  return computerChoice;
+};
+
+console.log();
+
+function compareChoices(playerSelection, computerSelection) {
+  if (
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "scissors" && computerSelection === "paper") ||
+    (playerSelection === "paper" && computerSelection === "rock")
+  ) {
+    return `You win! ${playerSelection} beats ${computerSelection}`;
+  } else if (
+    (computerSelection === "rock" && playerSelection === "scissors") ||
+    (computerSelection === "scissors" && playerSelection === "paper") ||
+    (computerSelection === "paper" && playerSelection === "rock")
+  ) {
+    return `You lose! ${computerSelection} beats ${playerSelection}!`;
+  } else if (playerSelection === computerSelection) {
+    return `You both got ${playerSelection}! Try again!`;
+  }
 }
+
+console.log(compareChoices("rock", getComputerChoice()));
