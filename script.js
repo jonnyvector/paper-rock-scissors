f4df682afa5631ce433f4ab26fdc61421ae6aae2
JONATHAN HICKS <jonny@teachon.org>
2023-11-01 09:48:07 +0700

    add UI elements and style

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+let playersChoice = "";
+const buttons = document.querySelectorAll("button");
+const startButton = document.querySelector("#start");
+
+let playerScore = 0;
+let computerScore = 0;
+
+// Computer selects randomly
 const getComputerChoice = () => {
   const randomNumber = Math.floor(Math.random() * 3);
   const gameChoicesArray = ["rock", "paper", "scissors"];
@@ -5,22 +13,13 @@ const getComputerChoice = () => {
   return computerChoice;
 };
 
-let playersChoice = "";
-
-const buttons = document.querySelectorAll("button");
-const startButton = document.querySelector("#start");
-
 const game = () => {
-  let playerScore = 0;
-  let computerScore = 0;
-
+  // assign player choice w/ click
   buttons.forEach((button) => {
     button.addEventListener("click", () => {
       playersChoice = button.value;
     });
   });
-
-  console.log(playersChoice);
 
   function compareChoices(playerSelection, computerSelection) {
     const playerSelectionLowerCase = playerSelection.toLowerCase();
@@ -42,23 +41,22 @@ const game = () => {
       (computerSelection === "paper" && playerSelectionLowerCase === "rock")
     ) {
       computerScore++;
+      console.log(`Computer score is ${computerScore}`);
       return `You lose! ${computerSelection} beats ${playerSelectionLowerCase}!`;
     } else if (playerSelectionLowerCase === computerSelection) {
       return `You both got ${playerSelectionLowerCase}! Try again!`;
     }
   }
 
-  for (let i = 1; i <= 5; i++) {
-    compareChoices(playersChoice, getComputerChoice());
-  }
+  compareChoices(playersChoice, getComputerChoice());
   if (playerScore > computerScore) {
-    console.log("Congratulations! You win");
+    // console.log("Congratulations! You win");
   } else {
-    console.log("Sorry, pal. You lose!");
+    // console.log("Sorry, pal. You lose!");
   }
-  console.log(
-    `The player's score is ${playerScore} and the computer's score is ${computerScore}!`
-  );
+  // console.log(
+  // `The player's score is ${playerScore} and the computer's score is ${computerScore}!`
+  // );
 };
 
 startButton.addEventListener("click", function () {
